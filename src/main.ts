1c79d451b20f187452b7c6af01dc36be56d5a01e
John A. Kallergis <j.a.kallergis@gmail.com>
2021-05-17 20:31:53 +0300

    fix: Fixed core action import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import core from '@actions/core'
+import {getInput, setFailed} from '@actions/core'
 import getGitCommits from './utils/getGitCommits'
 import {getCurrentVersion} from './utils/getCurrentVersion'
 import {extractCurrentChangelog} from './utils/extractCurrentChangelog'
@@ -7,8 +7,8 @@ import formatUnicorn from './utils/formatUnicorn'
 
 async function run(): Promise<void> {
   try {
-    const repo = core.getInput('repo')
-    const owner = core.getInput('owner')
+    const repo = getInput('repo')
+    const owner = getInput('owner')
     const ctx = {repo, owner}
 
     const version = getCurrentVersion()
@@ -20,7 +20,7 @@ async function run(): Promise<void> {
     fs.writeFileSync('./CHANGELOG.md', `${formattedLog}${currentLog}`)
   } catch (error) {
     console.log({error})
-    core.setFailed(error.message)
+    setFailed(error.message)
   }
 }
 
